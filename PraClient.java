183fbbcc63b87fa2b2a2117ecd89a8d05c36a141
“majidalmaadeed” <msm9mm@gmial.com>
2023-11-28 17:48:20 +0000

    Client and server 80% done

---
+++ PraClient.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class PraClient {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 12345);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println(in.readLine());
+            System.out.println(in.readLine());
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(in.readLine());
+            String searchPath = scanner.nextLine();
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(searchPath);
+
+            System.out.println(in.readLine());
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ PraServer.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
+public class PraServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("Server waiting for connections on port " + serverSocket.getLocalPort());
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connection established with " + clientSocket.getInetAddress());
+
+                int numOfCores = Runtime.getRuntime().availableProcessors();
+                ExecutorService executorService = Executors.newFixedThreadPool(numOfCores);
+
+                IntStream.range(0, numOfCores).forEach(i -> {
+                    executorService.execute(() -> {
+                        try {
+                            String os = System.getProperty("os.name");
+
+                            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                            out.println("Detected OS: " + os);
+                            out.println("Thread running on CPU core: " + Thread.currentThread().getId());
+
+                            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                            out.println("Enter the path to search in:");
+                            String searchPath = in.readLine();
+
+                            out.println("Searching in path: " + searchPath);
+
+                            Thread.sleep(2000);
+
+                            clientSocket.close();
+                        } catch (IOException | InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                });
+
+                executorService.shutdown();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
